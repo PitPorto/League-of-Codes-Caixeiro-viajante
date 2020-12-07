f359f8a0c85460cb4cf494971fe3b6be378e07b7
Piter Santos <piter_porto@id.uff.br>
2020-12-07 04:00:54 -0300

    Primeira função, matriz cidades

---
+++ Caixeiro Viajante.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+ 
+int ary[10][10],completed[10],n,cost=0;
+ 
+void takeInput()
+{
+	int i,j;
+ 
+	printf("Enter the number of villages: ");
+	scanf("%d",&n);
+ 
+	printf("\nEnter the Cost Matrix\n");
+ 
+	for(i=0;i < n;i++)
+	{
+		printf("\nEnter Elements of Row: %d\n",i+1);
+ 
+		for( j=0;j < n;j++)
+			scanf("%d",&ary[i][j]);
+ 
+		completed[i]=0;
+	}
+ 
+	printf("\n\nThe cost list is:");
+ 
+	for( i=0;i < n;i++)
+	
+
+		printf("\n");
+ 
+		for(j=0;j < n;j++)
+			printf("\t%d",ary[i][j]);
+	}
+
+	int main()
+	{
+	}
+	
